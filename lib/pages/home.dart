a19eada9262c27c31d175737602674e46e502001
bagasboyy <101851131+bagasboyy@users.noreply.github.com>
2023-05-08 10:44:52 +0700

    update tampilan homepage

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -27,142 +27,161 @@ class HomePage extends StatelessWidget {
             )
           ],
         ),
-        body: Column(
-          children: [
-            Center(
-              child: Column(
-                children: [
-                  Row(
-                    children: [
-                      Card(
-                        margin: EdgeInsets.all(30),
-                        color: Colors.transparent,
-                        elevation: 0.0,
-                        child: SizedBox(
-                          width: 350,
-                          height: 220,
-                          child: InkWell(
-                            onTap: () {
-                                        
-                              print("tapped");
-                            },
-                            child: Column(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: [
-                                const Text("Berita Terbaru", style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),),
-                                const SizedBox(
-                                  height: 10,
-                                ),
-                                Image.asset(
-                                  'assets/berita.jpeg', 
-                                ),   
-                              ],
+        body: SafeArea(
+          child: Column(
+            children: [
+              Center(
+                child: Column(
+                  children: [
+                    Row(
+                      children: [
+                        Container(
+                          margin: EdgeInsets.all(30),
+                          decoration: const BoxDecoration(
+                            color: Colors.red,
+                            borderRadius: BorderRadius.all(Radius.circular(30)
                             ),
                           ),
-                        ),
-                      ),
-                    ],
-                  ),
-                  const SizedBox(
-                        height: 70,
-                      ),
-                  Container(
-                    width: double.infinity,
-                    child: Column(
-                      children: [
-                        Text("Info Pilkada", style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold, color: Colors.black),),
-                        SingleChildScrollView(
-                          scrollDirection: Axis.horizontal,
-                          child: Row(
-                            children: [
-                              Card(
-                                margin: EdgeInsets.all(8),
-                                color: Colors.transparent,
-                                elevation: 0.0,
-                                child: SizedBox(
-                                  width: 230,
-                                  height: 150,
-                                  child: InkWell(
-                                    onTap: () {
-                                      Navigator.of(context).push(MaterialPageRoute(builder: (_) {
-                                        return PartaiPage() ;
-                                      }));
-                                      print("tapped");
-                                    },
-                                    child: Column(
-                                      mainAxisAlignment: MainAxisAlignment.center,
-                                      children: [
-                                        Image.asset(
-                                          'assets/logopartai.jpg', height: 120, width: 250, 
-                                        ),
-                                        const Text("Partai", style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),),
-                                      ],
-                                    ),
+                          child: SizedBox(
+                            width: 350,
+                            height: 250,
+                            child: InkWell(
+                              onTap: () {
+                                          
+                                print("tapped");
+                              },
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  const Text("Berita Terbaru", style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold, color: Colors.white),),
+                                  const SizedBox(
+                                    height: 10,
                                   ),
-                                ),
+                                  Image.asset(
+                                    'assets/berita.jpeg', 
+                                  ),   
+                                ],
                               ),
-                              Card(
-                                margin: EdgeInsets.all(8),
-                                color: Colors.transparent,
-                                elevation: 0.0,
-                                child: SizedBox(
-                                  width: 210,
-                                  height: 150,
-                                  child: InkWell(
-                                    onTap: () {
-                                      Navigator.of(context).push(MaterialPageRoute(builder: (_) {
-                                        return RekapVotingPage() ;
-                                      }));
-                                      print("tapped");
-                                    },
-                                    child: Column(
-                                      mainAxisAlignment: MainAxisAlignment.center,
-                                      children: [
-                                        Image.asset(
-                                          'assets/hasilvote.jpg', height: 120, width: 250,  
-                                        ),
-                                        const Text("Hasil Vote", style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),),
-                                      ],
-                                    ),
-                                  ),
-                                ),
-                              ),
-                              Card(
-                                margin: EdgeInsets.all(8),
-                                color: Colors.transparent,
-                                elevation: 0.0,
-                                child: SizedBox(
-                                  width: 200,
-                                  height: 150,
-                                  child: InkWell(
-                                    onTap: () {
-                                      Navigator.of(context).push(MaterialPageRoute(builder: (_) {
-                                        return AboutAppsPage() ;
-                                      }));
-                                      print("tapped");
-                                    },
-                                    child: Column(
-                                      mainAxisAlignment: MainAxisAlignment.center,
-                                      children: [
-                                        Image.asset(
-                                          'assets/infopemilu.jpeg', height: 120, width: 250, 
-                                        ),
-                                        const Text("Tentang Aplikasi", style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),),
-                                      ],
-                                    ),
-                                  ),
-                                ),
-                              ),
-                            ],
+                            ),
                           ),
                         ),
                       ],
                     ),
-                  ),
-                ],
-              ),
-            )
-          ],
+                    const SizedBox(
+                          height: 32,
+                        ),
+                    Container(
+                      width: double.infinity,
+                      height: 205,
+                      decoration: BoxDecoration(
+                        // color: Colors.red,
+                        borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(30),
+                          topRight: Radius.circular(30)
+                        ),
+                      ),
+                      child: Column(
+                        children: [
+                          SizedBox(
+                            height: 15,
+                          ),
+                          Text("Info Pilkada", style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),),
+                          SizedBox(
+                            height: 10,
+                          ),
+                          SingleChildScrollView(
+                            scrollDirection: Axis.horizontal,
+                            child: Row(
+                              children: [
+                                Card(
+                                  margin: EdgeInsets.all(8),
+                                  color: Colors.transparent,
+                                  elevation: 0.0,
+                                  child: SizedBox(
+                                    width: 200,
+                                    height: 130,
+                                    child: InkWell(
+                                      onTap: () {
+                                        Navigator.of(context).push(MaterialPageRoute(builder: (_) {
+                                          return PartaiPage() ;
+                                        }));
+                                        print("tapped");
+                                      },
+                                      child: Column(
+                                        mainAxisAlignment: MainAxisAlignment.center,
+                                        children: [
+                                          Image.asset(
+                                            'assets/logopartai.png', height: 100, width: 230, 
+                                          ),
+                                          const Text("Partai", style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),),
+                                        ],
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                                Card(
+                                  margin: EdgeInsets.all(8),
+                                  color: Colors.transparent,
+                                  elevation: 0.0,
+                                  child: SizedBox(
+                                    width: 200,
+                                    height: 130,
+                                    child: InkWell(
+                                      onTap: () {
+                                        Navigator.of(context).push(MaterialPageRoute(builder: (_) {
+                                          return RekapVotingPage() ;
+                                        }));
+                                        print("tapped");
+                                      },
+                                      child: Column(
+                                        mainAxisAlignment: MainAxisAlignment.center,
+                                        children: [
+                                          Image.asset(
+                                            'assets/hasilvote.png', height: 100, width: 230,  
+                                          ),
+                                          const Text("Hasil Vote", style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),),
+                                        ],
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                                Card(
+                                  margin: EdgeInsets.all(8),
+                                  color: Colors.transparent,
+                                  elevation: 0.0,
+                                  child: SizedBox(
+                                    width: 200,
+                                    height: 130,
+                                    child: InkWell(
+                                      onTap: () {
+                                        Navigator.of(context).push(MaterialPageRoute(builder: (_) {
+                                          return AboutAppsPage() ;
+                                        }));
+                                        print("tapped");
+                                      },
+                                      child: Column(
+                                        mainAxisAlignment: MainAxisAlignment.center,
+                                        children: [
+                                          Image.asset(
+                                            'assets/infopemilu.jpeg', height: 100, width: 230, 
+                                          ),
+                                          const Text("Tentang Aplikasi", style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),),
+                                        ],
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
         ),
       ),
       debugShowCheckedModeBanner: false,
